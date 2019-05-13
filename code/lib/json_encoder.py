933637da6241e4e5645d064636ea08ff23f2401d
Dmitriy Sidenko <dsidenko@zuzex.com>
2019-05-13 16:42:01 +0300

    PR-20. Change str to force_text

--- code/lib/json_encoder.py
+++ code/lib/json_encoder.py
@@ -6,14 +6,17 @@ from bson import ObjectId
 from rest_framework.renderers import JSONRenderer
 from rest_framework.utils import encoders
 from django.core.serializers.json import DjangoJSONEncoder
+#from django.utils.text import force_text
 
 class JSONEncoder(json.JSONEncoder):
     """JSONEncoder for import/export model"""
     def default(self, o):
         if isinstance(o, ObjectId):
-            return str(o)
+            #return str(o)
+            return force_text('%s' % (o))
         if isinstance(o, datetime):
-            return str(o)
+            #return str(o)
+            return force_text('%s' % (o))
         if isinstance(o, numpy.integer):
             return int(o)
         return json.JSONEncoder.default(self, o)
@@ -23,7 +26,8 @@ class JSONEncoderHttp(DjangoJSONEncoder):
     """JSONEncoder for custom JSONRender """
     def default(self, o):
         if isinstance(o, ObjectId):
-            return str(o)
+            #return str(o)
+            return force_text('%s' % (o))
         if isinstance(o, numpy.integer):
             return int(o)
         return super(JSONEncoderHttp, self).default(o)
@@ -33,7 +37,8 @@ class JSONEncoderRender(encoders.JSONEncoder):
     """JSONEncoder for custom JSONRender """
     def default(self, o):
         if isinstance(o, ObjectId):
-            return str(o)
+            #return str(o)
+            return force_text('%s' % (o))
         if isinstance(o, numpy.integer):
             return int(o)
         return encoders.JSONEncoder.default(self, o)
