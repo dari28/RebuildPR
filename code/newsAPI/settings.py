0f60d57b1e978d0c984616b2fa830308e1440f42
Dmitriy Sidenko <dsidenko@zuzex.com>
2019-05-27 16:29:10 +0300

    pr-16 timing of requests

--- code/newsAPI/settings.py
+++ code/newsAPI/settings.py
@@ -57,13 +57,13 @@ MIDDLEWARE = [
 
 if DEBUG:
     MIDDLEWARE.extend([
-        'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
- #       'django.contrib.auth.middleware.AuthenticationMiddleware',
-#        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#         'django.middleware.security.SecurityMiddleware',
+#         'django.contrib.sessions.middleware.SessionMiddleware',
+#         'django.middleware.common.CommonMiddleware',
+#         'django.middleware.csrf.CsrfViewMiddleware',
+#  #       'django.contrib.auth.middleware.AuthenticationMiddleware',
+# #        'django.contrib.messages.middleware.MessageMiddleware',
+#         'django.middleware.clickjacking.XFrameOptionsMiddleware',
         'pyinstrument.middleware.ProfilerMiddleware'
     ])
 
