76ad03f0250cb18be83735eafbfdb45959e17498
Egor Ostapenko <eostapenko@zuzex.com>
2019-06-17 14:08:54 +0300

    .

--- code/news.py
+++ code/news.py
@@ -15,7 +15,7 @@ countries = {
 
 languages = {'ar', 'en', 'cn', 'de', 'es', 'fr', 'he', 'it', 'nl', 'no', 'pt', 'ru', 'sv', 'ud'}
 
-languages_dict = [{'ar'}, {'en': "english"}, 'cn', 'de', 'es', 'fr', 'he', 'it', 'nl', 'no', 'pt', 'ru', 'sv', 'ud']
+languages_dict = {'ar': "Arabic", 'en': "English", 'cn': "Chinese", 'de': "German", 'es': "Spanish", 'fr': "French", 'he': "Hebrew", 'it': "Italian", 'nl': "Dutch", 'no': "Norwegian", 'pt': "Portuguese", 'ru': "Russian", 'sv': "Swedish", 'ud': "Urdu"}
 
 # languages_dict = {'ar': 'arabian', 'en': 'english', 'cn': , 'de', 'es', 'fr', 'he', 'it', 'nl', 'no', 'pt', 'ru', 'sv', 'ud'}
 
