c4141f42ddeb1796c12f55a46637782e9477365a
Egor Ostapenko <eostapenko@zuzex.com>
2019-06-26 10:56:10 +0300

    Update mongo_connection.py

--- code/lib/mongo_connection.py
+++ code/lib/mongo_connection.py
@@ -134,7 +134,7 @@ def recursive_geodata_find(params):
                 keys = list(address.keys())
                 level = addr_len - 1
                 tp = keys[0]
-                if not (level <= 5 or tp not in ['road', 'river', 'islet', 'island', 'stream', 'restaurant', 'cafe',
+                if not (level <= 5 or tp in ['road', 'river', 'islet', 'island', 'stream', 'restaurant', 'cafe',
                                        'peak', 'supermarket', 'guest_house', 'neighbourhood', 'water', 'hamlet']):
                     if (level == 0 or len(location._address.split(',')) == 1) & (len(list(mongodb.location.find({'place_id': location.raw['place_id']}))) == 0):
                         added_id = mongodb.location.insert_one({'place_id': location.raw['place_id'],
