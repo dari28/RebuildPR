b79da76d7a24c249a24b81e8e45854cf2bc50226
Dmitriy Sidenko <dsidenko@zuzex.com>
2019-07-02 08:04:36 +0300

    PR-43. No delete single-quotes

--- code/lib/mongo_connection.py
+++ code/lib/mongo_connection.py
@@ -868,8 +868,8 @@ class MongoConnection(object):
         content = re.sub(r'[ ]{2,}', ' ', content)  # change 2+ spaces to one space
         content = re.sub(r'[\u201c\u201d\u201e\u201f"]', "'", content, flags=re.U)  # change double-quotes to normal double-quotes
         content = re.sub(r"[\u2018\u2019\u201a\u201b']", "'", content, flags=re.U)  # change single-quotes to normal single-quotes
-        content = re.sub(r" '", " ", content)  # remove single-quotes with space before
-        content = re.sub(r"' ", " ", content)  # remove single-quotes with space after
+        # content = re.sub(r" '", " ", content)  # remove single-quotes with space before
+       #  content = re.sub(r"' ", " ", content)  # remove single-quotes with space after
         content = re.sub(r"[/]+", "/", content)  # get only one /
         content = re.sub(r" /", " ", content)  # remove / with space before
         content = re.sub(r"/ ", " ", content)  # remove / with space after
